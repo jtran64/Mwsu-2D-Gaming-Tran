00087f70652eace571d0ae3450fe5afce905db6e
jtran64 <jbtran2973@hotmail.com>
2016-07-05 16:20:41 -0400

    Updating play.js

--- Program-6/js/play.js
+++ Program-6/js/play.js
@@ -1,6 +1,5 @@
 
 //====================================================================
-
 var playState = {
     /** 
     * Establish eureca client and setup some globals
@@ -47,6 +46,12 @@ var playState = {
         // Reference to our eureca so we can call functions back on the server
         var eurecaProxy;
         
+		this.client.ready(function (serverProxy) {
+             // Local reference to the server proxy to be 
+             // used in other methods within this module.
+             eurecaProxy = serverProxy;
+
+        });
         /**
         * Fires on initial connection
         */
@@ -59,12 +64,12 @@ var playState = {
         * we will set a local variable to the "serverProxy" 
         * sent back by the server side.
         */
-        this.client.ready(function (serverProxy) {
+        /*this.client.ready(function (serverProxy) {
              // Local reference to the server proxy to be 
              // used in other methods within this module.
              eurecaProxy = serverProxy;
 
-        });
+        });*/
         
         /**
         * This sets the players id that we get from the server
@@ -81,7 +86,7 @@ var playState = {
             globals.dude = new player(id, game,eurecaProxy);
             
             // Put instance of "dude" into list
-            globals.playerList[id] = globals.dude.state;
+            globals.playerList[id] = globals.dude;//.state;
             
             //Send state to server
             eurecaProxy.initPlayer(id,globals.dude.state);
@@ -93,10 +98,7 @@ var playState = {
             globals.playerReady = true;
             
             // Send a handshake to say hello to other players.
-            eurecaProxy.handshake();
-            
-
-            
+            eurecaProxy.handshake(); 
         }
         /**
         * Called from server when another player "disconnects"
@@ -114,20 +116,29 @@ var playState = {
         this.client.exports.spawnEnemy = function(id, enemy_state){
             
             if (id == globals.myId){
+				console.log("This is me");
                 return; //this is me
             }
             
             //if the id doesn't exist in your local table
             // then spawn the enemy
-
-            console.log('Spawning New Player');
-            
-            console.log(enemy_state);
-    
-            globals.playerList[id] = enemy_state;
-            
-            console.log(globals.playerList);
-
+			if(!globals.playerList[id]){
+				var enemy = new player(id, game,eurecaProxy);
+				enemy.state = enemy_state;
+				enemy.sprite.x = enemy_state.x;
+				enemy.sprite.y = enemy_state.y;
+				enemy.sprite.tint = enemy_state.tint;
+				globals.playerList[id] = enemy;
+			}
+			
+			console.log('Spawning New Player');
+				
+			console.log(enemy_state);
+		
+			//globals.playerList[id] = enemy_state;
+				
+			console.log(globals.playerList);
+			
         }
 
         /**
@@ -135,7 +146,7 @@ var playState = {
         * state. 
         */       
         this.client.exports.updateState = function(id,player_state){
-            console.log(id,player_state);
+            //console.log(id,player_state);
             
             // Don't do anything if its me
             if(globals.myId == id){
@@ -145,9 +156,11 @@ var playState = {
             // If player exists, update that players state. 
             if (globals.playerList[id])  {
                 globals.playerList[id].state = player_state;
+				globals.playerList[id].updateState(id, player_state);
             }
             
             //now how do we update everyone??
+			
          }
          
         
@@ -165,7 +178,7 @@ var playState = {
         game.state.start('menu');
     },
 };
-
+/*
 var player = function (index, game,proxyServer) {
     
     var x;                      // x coord
@@ -277,4 +290,109 @@ var player = function (index, game,proxyServer) {
         update : update,
         kill : kill
     };
+};*/
+
+function player(index, game, proxyServer) {
+	this.player_id = index;
+    
+    this.proxy = proxyServer;
+    
+    this.sprite = game.add.sprite(this.x, this.y, 'dude');
+    
+    this.upKey = game.input.keyboard.addKey(Phaser.Keyboard.UP);
+    this.downKey = game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
+    this.leftKey = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
+    this.rightKey = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
+
+    this.health = 30;
+	    
+    this.state = {}//= {alive:true,tint_val:this.tint_val,x:this.x,y:this.y};
+    this.x = 0;
+    this.y = 0;
+    this.alive = true;
+    this.tint = Math.random() * 0xffffff;
+    this.sprite.tint = this.tint;
+    this.sprite.id = index;
+    this.state.alive = true;
+    this.startTime = game.time.time;
+};
+
+player.prototype.update = function() {
+		this.state.tint = this.tint;
+		//this.sprite.tint = this.tint;
+        this.state.x = this.sprite.x;
+        this.state.y = this.sprite.y;
+		//console.log(this.state.x, this.state.y);
+        this.state.alive = this.alive;
+        this.state.health = this.health;
+    
+        // Send your own state to server on your update and let
+        // it do whatever with it. 
+			this.proxy.handleState(this.player_id,this.state);
+
+        if (this.upKey.isDown)
+        {
+            this.sprite.y-=3;
+        }
+        else if (this.downKey.isDown)
+        {
+            this.sprite.y+=3;
+        }
+
+        if (this.leftKey.isDown)
+        {
+            this.sprite.x-=3;
+        }
+        else if (this.rightKey.isDown)
+        {
+            this.sprite.x+=3;
+        } 
+    
+        old_x = this.sprite.x;
+        old_y = this.sprite.y;
+};
+
+player.prototype.kill = function() {
+	this.alive = false;
+    this.sprite.kill();
+};
+
+player.prototype.render = function() {
+	game.debug.text( "This is debug text", 100, 380 );
+};
+
+player.prototype.updateState = function(enemy_id,state) {
+	//if(game.time.time - this.startTime > 2000){
+            console.log(game.time.time);
+            for(s in state){
+                console.log(state[s]);
+				this.state[s] = state[s];
+				this.sprite.x = this.state.x;
+				this.sprite.y = this.state.y;
+				this.sprite.tint = this.state.tint;
+            }
+			
+            this.startTime = game.time.time;
+        //}	
+};
+
+player.prototype.init = function(index, game,proxyServer, state) {
+	    this.player_id = state.index;
+    
+        this.proxy = proxyServer;
+    
+        this.sprite = game.add.sprite(state.x, state.y, 'dude');
+
+        this.health = state.health;
+	    
+        this.state = state;
+		this.x = state.x;
+        this.y = state.y;
+        this.alive = true;
+        this.tint = state.tint;
+        this.sprite.tint = state.tint;
+        this.sprite.id = index;
+        this.state.alive = true;
+        this.startTime = game.time.time;
+        
 };
